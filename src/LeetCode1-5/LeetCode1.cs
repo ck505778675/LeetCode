f16008211652feabb4c0c36253c5cbb31e8b0ec6
程序人生1992 <505778675@qq.com>
2018-01-27 11:30:21 +0800

    LeetCode第一题提交

---
+++ src/LeetCode1-5/LeetCode1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode1_5
+{
+    public class LeetCode1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+
+            throw new ArgumentException("No two sum solution");
+        }
+
+        public int[] TwoSum2(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                dic[nums[i]] = i; //不能使用add来添加，避免key重复报错
+            }
+
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                int temp = target - nums[i];
+                if (dic.ContainsKey(temp) && dic[temp] != i)
+                    return new int[] { i, dic[temp] };
+            }
+
+            throw new ArgumentException("No two sum solution");
+
+
+        }
+    }
+}
--- src/LeetCode1-5/UnitTest1.cs
+++ src/LeetCode1-5/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
 
 namespace LeetCode1_5
 {
@@ -6,8 +8,70 @@ namespace LeetCode1_5
     public class UnitTest1
     {
         [TestMethod]
+        public void TestMethod0()
+        {
+            int[] nums = new int[10000000];
+            Random rd = new Random();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rd.Next(1, 31);
+            }
+            Stopwatch sw = new Stopwatch();
+            LeetCode1 leetCode1 = new LeetCode1();
+
+            sw.Start();
+            foreach (var num in leetCode1.TwoSum(nums, 59))
+            {
+                Console.WriteLine($"{num}:{nums[num]}");
+            }
+            sw.Stop();
+            Console.WriteLine($"方法一使用时间(ms)为:{sw.ElapsedMilliseconds}");
+
+            sw.Restart();
+            foreach (var num in leetCode1.TwoSum2(nums, 59))
+            {
+                Console.WriteLine($"{num}:{nums[num]}");
+            }
+            sw.Stop();
+            Console.WriteLine($"方法二使用时间(ms)为:{sw.ElapsedMilliseconds}");
+        }
+
+
+        [TestMethod]
         public void TestMethod1()
         {
+            int[] nums = new int[100000];
+            Random rd = new Random();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rd.Next(1, 31);
+            }
+            
+            LeetCode1 leetCode1 = new LeetCode1();
+            
+
+            foreach (var num in leetCode1.TwoSum(nums, 59))
+            {
+                Console.WriteLine($"{num}:{nums[num]}");
+            }
+
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] nums = new int[100000];
+            Random rd = new Random();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rd.Next(1, 31);
+            }
+
+            LeetCode1 leetCode1 = new LeetCode1();
+            foreach (var num in leetCode1.TwoSum2(nums, 59))
+            {
+                Console.WriteLine($"{num}:{nums[num]}");
+            }
         }
     }
 }

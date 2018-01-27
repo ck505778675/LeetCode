c0f31a40acfbf67e4b746de30ecb46485ec390e8
程序人生1992 <505778675@qq.com>
2018-01-27 10:35:56 +0800

    LeetCode创建

---
+++ src/LeetCode1-5/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode1_5
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

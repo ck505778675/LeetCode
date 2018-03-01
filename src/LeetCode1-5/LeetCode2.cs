80f949d59dcafa43b7f90e03e80c696a1c670c94
程序人生1992 <505778675@qq.com>
2018-03-02 00:20:19 +0800

    新增第二题

---
+++ src/LeetCode1-5/LeetCode2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode1_5
+{
+    public class LeetCode2
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var newNode = new ListNode((l1.val + l2.val) % 10);
+            var node = newNode;
+            var temp = (l1.val + l2.val) / 10 > 0 ? 1 : 0;
+            while (l1.next != null || l2.next != null)
+            {
+                l1 = l1.next ?? new ListNode(0);
+                l2 = l2.next ?? new ListNode(0);
+                node.next = new ListNode((l1.val + l2.val + temp) % 10);
+                node = node.next;
+                temp = (l1.val + l2.val + temp) / 10 > 0 ? 1 : 0;
+
+            }
+            if (temp > 0)
+            {
+                node.next = new ListNode(1);
+
+            }
+            return newNode;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ src/LeetCode1-5/LeetCode2_UntiTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode1_5
+{
+    [TestClass]
+    public class LeetCode2_UntiTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ListNode l1 = new ListNode(2) { next = new ListNode(4) { next = new ListNode(3) { next = new ListNode(9) } } };
+            ListNode l2 = new ListNode(8) { next = new ListNode(5) { next = new ListNode(6) } };
+            LeetCode2 leetCode2 = new LeetCode2();
+            ListNode resultNode = leetCode2.AddTwoNumbers(l1, l2);
+            while (resultNode != null)
+            {
+                Console.WriteLine(resultNode.val);
+                resultNode = resultNode.next;
+            }
+
+        }
+    }
+}

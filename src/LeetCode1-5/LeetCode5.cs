b26d7504df3c302d66dae72c95cd8aa2cf9628d7
程序人生1992 <505778675@qq.com>
2018-03-05 11:25:49 +0800

    完成第五题

--- src/LeetCode1-5/LeetCode5.cs
+++ src/LeetCode1-5/LeetCode5.cs
@@ -8,23 +8,80 @@ namespace LeetCode1_5
     {
         public string LongestPalindrome(string s)
         {
-            var data = new List<char>();
-            string tempStr = "";
-            int temp = 0;
             if (s.Length == 1)
                 return s;
-            for (int i = 0; i < s.Length; i++)
+            int maxLength = 0;
+            string str = s.Substring(0, 1);
+            for (int index = 1; index < s.Length; index++)
             {
-                
-                int index = data.IndexOf(s[i]);
-                data.Add(s[i]);
-                if (index > -1 && (i - index + 1) > temp)
+
+                if (s[index - 1] == s[index])
                 {
-                    temp = i - index + 1;
-                    tempStr = s.Substring(index, temp);
+                    int i = index;
+                    int j = index - 1;
+                    while (i >= 1 && j <= s.Length - 2 && s[i - 1] == s[j + 1])
+                    {
+                        i--;
+                        j++;
+                    }
+                    if (j - i + 1 > maxLength)
+                    {
+                        maxLength = j - i + 1;
+                        str = s.Substring(i, maxLength);
+                    }
+
                 }
+                if (index < s.Length - 1 && s[index - 1] == s[index + 1])
+                {
+                    int i = index;
+                    int j = index;
+                    while (i >= 1 && j <= s.Length - 2 && s[i - 1] == s[j + 1])
+                    {
+                        i--;
+                        j++;
+                    }
+                    if (j - i + 1 > maxLength)
+                    {
+                        maxLength = j - i + 1;
+                        str = s.Substring(i, maxLength);
+                    }
+
+                }
+
             }
-            return tempStr;
+            return str;
+        }
+
+        private int lo, maxLen;
+
+        public string LongestPalindrome1(string s)
+        {
+
+            if (s.Length < 2)
+                return s;
+            lo = 0;
+            maxLen = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                ExtendPalindrome(s, i, i - 1);
+                ExtendPalindrome(s, i, i);
+            }
+            return s.Substring(lo, maxLen);
+
+        }
+        public void ExtendPalindrome(string s, int j, int k)
+        {
+            while (j >= 1 && k <= s.Length - 2 && s[j - 1] == s[k + 1])
+            {
+                j--;
+                k++;
+            }
+            if (k - j + 1 > maxLen)
+            {
+                lo = j;
+                maxLen = k - j + 1;
+
+            }
         }
     }
 }
--- src/LeetCode1-5/LeetCode5_UnitTest.cs
+++ src/LeetCode1-5/LeetCode5_UnitTest.cs
@@ -13,7 +13,18 @@ namespace LeetCode1_5
         {
             LeetCode5 leetCode5 = new LeetCode5();
             Console.WriteLine(leetCode5.LongestPalindrome("babad"));
-            Console.WriteLine(leetCode5.LongestPalindrome("cbbd"));
+            Console.WriteLine(leetCode5.LongestPalindrome("aa"));
+            Console.WriteLine(leetCode5.LongestPalindrome("a"));
+            Console.WriteLine(leetCode5.LongestPalindrome("aaa"));
+        }
+        [TestMethod]
+        public void LeetCode5_UnitTest_Method2()
+        {
+            LeetCode5 leetCode5 = new LeetCode5();
+            Console.WriteLine(leetCode5.LongestPalindrome1("babad"));
+            Console.WriteLine(leetCode5.LongestPalindrome1("aa"));
+            Console.WriteLine(leetCode5.LongestPalindrome1("a"));
+            Console.WriteLine(leetCode5.LongestPalindrome1("aaa"));
         }
     }
 }
